0a20ea20927910a1febebda939d911319f11ec2a
erikbaer <baerdata.ger@googlemail.com>
2020-08-11 21:50:09 +0200

    create mock database

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
-const express = require('express')
-const expressGraphQL = require('express-graphql')
-const app = express()
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql')
+
+const {authors, books} = require ('./database')
+
+
+const app = express();
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+         fields: () => ({
+             message: {type: GraphQLString,
+             resolve: () => 'Hello World'
+             }
+         })
+    })
+})
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+      schema,
+    graphiql: true,
+  }),
+);
 
 
 app.listen(5000, () => console.log('Server Running on Port 5000'))
