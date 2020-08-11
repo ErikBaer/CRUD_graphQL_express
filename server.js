0fe9267d5bcffa0560c5f00b33f14d0e3e6fd974
erikbaer <baerdata.ger@googlemail.com>
2020-08-11 23:14:46 +0200

    create Mutation to addAuthor

--- server.js
+++ server.js
@@ -97,6 +97,18 @@ const RootMutationType = new GraphQLObjectType({
               books.push(book)
               return book
             }
+          },
+          addAuthor: {
+            type: AuthorType,
+            description: 'Add an Author',
+            args: {
+              name: { type: GraphQLNonNull(GraphQLString) }
+            },
+            resolve: (parent, args) => { //this is where you do Database operations
+              const author = { id: authors.length + 1, name: args.name }
+              authors.push(author)
+              return author
+            }
           }
     })
 })
